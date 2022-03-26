23b0bc9e674b88b853c137b81fbead3c3be7fd0a
Johnathan Clementi <johnatawnclementawn@users.noreply.github.com>
2022-03-26 02:17:35 -0400

    Move files around and collect more via scraping

--- storymap/js/index.js
+++ storymap/js/index.js
@@ -1,7 +1,29 @@
 /* globals showdown */
 
-let map = L.map('map').setView([0, 0], 0);
+const map = L.map('map').setView([41.6934242, -73.9324951], 8);
+const countiesLayer = L.layerGroup().addTo(map);
 
-L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 18,
+  ext: 'png',
 }).addTo(map);
+
+/* Load Hudson Valley Counties */
+// let showHudValCounties = () => {
+countiesLayer.clearLayers();
+
+const dataFileName = '../data/artTrailSites.json';
+fetch(dataFileName)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    c = L.geoJSON(data)
+      .bindTooltip(layer => layer.feature.properties.name)
+      .addTo(map);
+    console.log(c);
+  });
+// };
+
