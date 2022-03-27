026de9de7d0e7001f7669b15b459e75632dc4a44
Johnathan Clementi <johnatawnclementawn@users.noreply.github.com>
2022-03-27 16:31:16 -0400

    Move to scrolling form

--- storymap/js/index.js
+++ storymap/js/index.js
@@ -1,7 +1,8 @@
 /* globals showdown */
 
-const map = L.map('map').setView([41.6934242, -73.9324951], 8);
-const countiesLayer = L.layerGroup().addTo(map);
+let map = L.map('map').setView([41.6934242, -73.9324951], 8);
+let layerGroup = L.layerGroup().addTo(map);
+let lifeCollection = { features: [] };
 
 L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
@@ -11,19 +12,100 @@ L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg', {
   ext: 'png',
 }).addTo(map);
 
-/* Load Hudson Valley Counties */
-let showHudValCounties = () => {
-  countiesLayer.clearLayers();
+let currentSlideIndex = 0;
 
-  const dataFileName = '../data/hudsonRiverValley.geojson';
-  fetch(dataFileName)
-    .then(response => response.json())
+const slidesDiv = document.querySelector('.slides');
+
+function updateMap(collection) {
+  layerGroup.clearLayers();
+  const geoJsonLayer = L.geoJSON(collection, { pointToLayer: (p, latlng) => L.marker(latlng) })
+    .bindTooltip(l => l.feature.properties.label)
+    .addTo(layerGroup);
+
+  return geoJsonLayer;
+}
+
+function makeEraCollection(era) {
+  return {
+    type: 'FeatureCollection',
+    features: lifeCollection.features.filter(f => f.properties.era === era),
+  };
+}
+
+function syncMapToSlide(slide) {
+  const collection = slide.era ? makeEraCollection(slide.era) : lifeCollection;
+  const layer = updateMap(collection);
+
+  function handleFlyEnd() {
+    if (slide.showpopups) {
+      layer.eachLayer(l => {
+        l.bindTooltip(l.feature.properties.label, { permanent: true });
+        l.openTooltip();
+      });
+    }
+    map.removeEventListener('moveend', handleFlyEnd);
+  }
+
+  map.addEventListener('moveend', handleFlyEnd);
+  if (slide.bounds) {
+    map.flyToBounds(slide.bounds);
+  } else if (slide.era) {
+    map.flyToBounds(layer.getBounds());
+  }
+}
+
+function syncMapToCurrentSlide() {
+  const slide = slides[currentSlideIndex];
+  syncMapToSlide(slide);
+}
+
+function initSlides() {
+  const converter = new showdown.Converter({ smartIndentationFix: true });
+
+  slidesDiv.innerHTML = '';
+  for (const [index, slide] of slides.entries()) {
+    const slideDiv = htmlToElement(`
+      <div class="slide" id="slide-${index}">
+        <h2>${slide.title}</h2>
+        ${converter.makeHtml(slide.content)}
+      </div>
+    `);
+    slidesDiv.appendChild(slideDiv);
+  }
+}
+
+function loadLifeData() {
+  fetch('data/journey.json')
+    .then(resp => resp.json())
     .then(data => {
-      console.log(data);
-      c = L.geoJSON(data)
-        .bindTooltip(layer => layer.feature.properties.name)
-        .addTo(map);
-      // console.log(c);
+      lifeCollection = data;
+      syncMapToCurrentSlide();
     });
-};
+}
 
+function calcCurrentSlideIndex() {
+  const scrollPos = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const slideDivs = document.getElementsByClassName('slide');
+
+  let i;
+  for (i = 0; i < slideDivs.length; i++) {
+    const slidePos = slideDivs[i].offsetTop;
+    if (slidePos - scrollPos - windowHeight > 0) {
+      break;
+    }
+  }
+
+  if (i === 0) {
+    currentSlideIndex = 0;
+  } else if (currentSlideIndex != i - 1) {
+    currentSlideIndex = i - 1;
+    syncMapToCurrentSlide();
+  }
+}
+
+document.addEventListener('scroll', calcCurrentSlideIndex);
+
+initSlides();
+syncMapToCurrentSlide();
+loadLifeData();
--- storymap/js/slides.js
+++ storymap/js/slides.js
@@ -1,32 +1,154 @@
-const slide1 = 0;
-
 const slides = [
   {
-    title: "This is my slide title",
+    title: "The Hudson River School of Art",
     content: `
-      Here is some sample content for this slide.
+      The Hudson River School of Art was a mid-19th century American art movement 
+      influenced by Romanticism and characterized by sweeping landscapes depicting 
+      nature and humans coexisting peacefully. There are two recognized generations 
+      of artists belonging to the Hudson River School of Art. The first generation 
+      primarily focused on the Hudson River Valley, including the Adirondak, Catskill, 
+      and White Mountains, whereas the second generation expanded to the American West,
+      New England, The Maritimes of Canada, and South America. This exploration 
+      will primarily cover first generation painters.      
     `,
+    era: null,
     bounds: [
       [
-        -76.783447265625,
-        39.884450178234395
-      ],
-      [
-        -71.35620117187499,
-        39.884450178234395
-      ],
-      [
-        -71.35620117187499,
-        43.45291889355465
-      ],
-      [
-        -76.783447265625,
-        43.45291889355465
-      ],
-      [
-        -76.783447265625,
-        39.884450178234395
+        [
+          40.49709237269567,
+          -80.15625
+        ],
+        [
+          40.49709237269567,
+          -70.3564453125
+        ],
+        [
+          45.38301927899065,
+          -70.3564453125
+        ],
+        [
+          45.38301927899065,
+          -80.15625
+        ],
+        [
+          40.49709237269567,
+          -80.15625
+        ]
       ]
-    ],
+    ]
+  },
+
+  {
+    title: 'Early Days',
+    content: `
+      Mjumbe was born in San Jose, California. He lived with his family first in
+      **San Jose**, then **Santa Cruz**, briefly in **Oceanside**, and finally
+      in **Riverside**.
+    `,
+    era: 'early life',
+    showpopups: true,
+  },
+
+  {
+    title: 'Move to Philadelphia',
+    content: `
+      When Mjumbe was 11, his family moved across the country to Philadelphia.
+      They lived in what would later be knows as the **Cedar Park**
+      neighborhood, and Mjumbe attended **J.R. Masterman** for middle and high
+      school.
+    `,
+    era: 'formative',
+    showpopups: true,
+  },
+
+  {
+    title: 'College',
+    content: `
+      Mjumbe went back to California for college. He graduated with a computer
+      science degree from **Harvey Mudd College** in **Claremont, California**.
+      After college, Mjumbe moved back to the Cedar Park neighborhood and worked
+      for a few years at the University of Pennsylvania, making tools for
+      modeling and simulation in the social sciences.
+    `,
+    era: 'college',
+    showpopups: true,
+  },
+
+  {
+    title: 'Code for America',
+    content: `
+      Mjumbe was a Code for America fellow in 2011, living and working primarily
+      from **Oakland, CA**. He worked on projects with the cities of Philadelphia
+      and Seattle.
+    `,
+    era: 'cfa',
+    showpopups: true,
+  },
+
+  {
+    title: 'OpenPlans',
+    content: `
+      After Code for America, Mjumbe went to work for OpenPlans, a NYC-based
+      non-profit making tools to help citizens get involved in the urban
+      planning process. He would usually work remotely from **Independents Hall**,
+      making occasional trips to the NYC office from his home in **Yorktown** via
+      the **Temple University train station**.
+    `,
+    era: 'openplans',
+    showpopups: true,
+  },
+
+  {
+    title: 'City of Philadelphia',
+    content: `
+      Mjumbe then went to work for the **City of Philadelphia** in the Office of
+      Open Data and Digital Transformation. During this period he lived near
+      Temple University in the **Hartranft** neighborhood.
+    `,
+    era: 'city of phila',
+    showpopups: true,
+  },
+
+  {
+    title: 'Stepwise',
+    content: `
+      Mjumbe left the city of Philadelphia to work on a startup called Stepwise
+      with a former coworker and friend. They made tools to help investors get
+      a fuller picture of potential investment properties. They would often work
+      out of the **First Round Capital** offices. For a time Mjumbe lived in the
+      **Woodland Terrace** neighborhood, and later in an apartment overlooking
+      **Malcolm X Park**.
+    `,
+    era: 'stepwise',
+    showpopups: true,
+  },
+
+  {
+    title: 'International Conference on Appropriate Technology',
+    content: `
+      Mjumbe has been involved with the planning of the International Conference
+      on Appropriate Technology since 2012. This conference has taken him to
+      locations around Africa.
+    `,
+    era: 'inat',
+    showpopups: true,
+  },
+
+  {
+    title: 'Travels',
+    content: `
+      Additionally, Mjumbe has been lucky to spend time in several places around
+      the world.
+    `,
+    era: 'travels',
+    showpopups: true,
+  },
+
+  {
+    title: 'Present Day',
+    content: `
+      Mjumbe currently lives in **East Passyunk**. Thanks for taking this journey!
+    `,
+    era: 'present day',
   },
 ];
